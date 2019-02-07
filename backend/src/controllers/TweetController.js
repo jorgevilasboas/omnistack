18989e5668e8b4e6f3a473c94beeea36a11b851d
jorgevilasboas <jorge.sergipe@gmail.com>
2019-02-07 17:28:40 -0300

    Finalize backend

--- backend/src/controllers/TweetController.js
+++ backend/src/controllers/TweetController.js
@@ -8,6 +8,7 @@ module.exports = {
 
     async store(req, res){
         const tweet = await Tweet.create(req.body);
+        req.io.emit('tweet', tweet);
         return res.json(tweet);
     }
 };
